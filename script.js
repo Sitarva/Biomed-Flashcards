3528c562dfc83750f05bb7be8ae718b5fd950abb
Sitarva <168885163+Sitarva@users.noreply.github.com>
2025-08-22 10:20:17 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 // ---------------------------
 // Supabase Integration
 // ---------------------------
-const supabaseUrl = "https://ucqoiltqcblrwkltglos.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVjcW9pbHRxY2JscndrbHRnbG9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU3Nzc1NTUsImV4cCI6MjA3MTM1MzU1NX0.d9nusguupaLupLRa1Yn7pBAgzJ9d2eU4Sx-SrgRAFcI";
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+const SUPABASE_URL = "https://ucqoiltqcblrwkltglos.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVjcW9pbHRxY2JscndrbHRnbG9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU3Nzc1NTUsImV4cCI6MjA3MTM1MzU1NX0.d9nusguupaLupLRa1Yn7pBAgzJ9d2eU4Sx-SrgRAFcI";
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 const IMAGES_BUCKET = "flashcards-images";
 
 // ---------------------------
